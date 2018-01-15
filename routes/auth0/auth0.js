ea3c3ba07be9cb1893ccade3589460a935944812
Gangplank <duc71098@gmail.com>
2018-01-15 12:19:08 +0700

    Adding routes

---
+++ routes/auth0/auth0.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+import config from './authConfig';
+
+const {
+  AUTH0_JWKS_URI,
+  AUTH0_AUDIENCE,
+  AUTH0_ISSUER,
+  AUTH0_ALGORITHMS
+} = config;
+
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: AUTH0_JWKS_URI
+    }),
+    audience: AUTH0_AUDIENCE,
+    issuer: AUTH0_ISSUER,
+    algorithms: [AUTH0_ALGORITHMS]
+});
+
+export { authCheck };
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,52 @@
 'use strict'
 
 import express from 'express';
+import { authCheck } from './auth0/auth0';
+import jwtAuthz from 'express-jwt-authz';
 
+const scopeCheck = jwtAuthz(['admin']);
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// TODO: add authCheck and scopeCheck when ready
+
+// Add new link
+router.put('/api/add', (req, res) => {
+  res.json('Add new link');
 });
 
-module.exports = router;
+// Update link
+router.post('/api/update', (req, res) => {
+  res.json('Update link');
+});
+
+// Delete link
+router.delete('/api/delete', (req, res) => {
+  res.json('Delete link');
+});
+
+// Parse web to add link
+router.put('/api/parse', (req, res) => {
+  res.json('Parse link');
+});
+
+// Request a specific number of link, looks like Google
+router.get('/api/page/:num', (req, res) => {
+  res.json(`Page ${req.params.num}`);
+});
+
+// Request links using search engine :v
+router.post('/api/search', (req, res) => {
+  res.json('Search');
+});
+
+// Request instant backup
+router.post('/api/backup', (req, res) => {
+  res.json('Manual backup');
+});
+
+// Request instant log transfer
+router.post('api/log', (req, res) => {
+  res.json('Log transfer');
+});
+
+export default router;
