4d54dece1f0ba2b0d2a374b282a1f78e22198aed
Gangplank <duc71098@gmail.com>
2018-01-16 21:19:33 +0700

    Add all model functions

--- models/models.js
+++ models/models.js
@@ -10,3 +10,93 @@ const pgp = pg({
 // Comment this when deploy
 const connectionString = 'postgres://localhost:5432/link';
 const db = pgp(connectionString || process.env.DATABASE_URL);
+
+// Parse condition 'where' or 'set' to query
+const parse = function parseToQuery(condition, connector) {
+  return Object.keys(condition).map((key) => {
+    return `${key}='${condition[key]}'`;
+  }).join(connector);
+}
+
+// All this file will export
+export { scanTable, addToTable, updateInTable, delFromTable };
+
+                          //  READ  //
+async function scanTable({ table, limit, offset = 0, condition = {} }) {
+  let result = [];
+
+  // Parse condition to query string
+  let notEmpty = Object.keys(condition).length !== 0;
+  let query = notEmpty ? `where ${parse(condition, ' and ')}` : '';
+
+  // Parse limit and offset to set range
+  let range = (limit) ? `limit ${limit} offset ${offset}` : '';
+
+  // Await db to respond and return result
+  try {
+    result = await db.any(`select * from ${table} ${query} ${range}`);
+  } catch (e) {
+    console.log(e);
+    result = [];
+  }
+
+  return result;
+}
+
+                          //  CREATE  //
+async function addToTable({ table, data }) {
+  let result = true;
+
+  // Parse keys and values from data
+  let keys = Object.keys(data).join(', ');
+  let values = Object.values(data).map(val => `'${val}'`).join(', ');
+
+  // Await db to respond and return result
+  try {
+    await db.none(`insert into ${table} (${keys}) values (${values})`);
+  } catch (e) {
+    console.log(e);
+    result = false;
+  }
+
+  return result;
+}
+
+                          //  UPDATE  //
+async function updateInTable({ table, changes, condition }) {
+  let result = true;
+
+  // Parse changes to format `key='value'`
+  let update = parse(changes, ', ');
+
+  // Parse condition
+  let query = parse(condition, ' and ');
+
+  // Await db to respond and return result
+  try {
+    await db.none(`update ${table} set ${update} where ${query}`);
+  } catch (e) {
+    console.log(e);
+    result = false;
+  }
+
+  return result;
+}
+
+                          //  REMOVE  //
+async function delFromTable({table, condition}) {
+  let result = true;
+
+  // Parse condition
+  let query = parse(condition, ' and ');
+
+  // Await db to respond and return result
+  try {
+    await db.none(`delete from ${table} where ${query}`);
+  } catch (e) {
+    console.log(e);
+    result = false;
+  }
+
+  return result;
+}
