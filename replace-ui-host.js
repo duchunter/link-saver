be3015d55a7f8ae8ff0349200da98dcc0781653b
duchunter <duc71098@gmail.com>
2021-03-25 23:44:22 +0700

    upgrade ui + fix minor bug

--- replace-ui-host.js
+++ replace-ui-host.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const fs = require('fs')
 
-const jsFolder = './dist/static/js'
+const jsFolder = './dist/js'
 const isDev = process.env.NODE_ENV !== 'prod'
 const localhost = 'http://localhost:3000'
+const localhostClient = 'http://localhost:8080'
 const prodHost = 'https://gp-linksaver.herokuapp.com'
 
 fs.readdir(jsFolder, (err, files) => {
@@ -18,13 +19,14 @@ fs.readdir(jsFolder, (err, files) => {
                 throw err
             }
             
-            let newContent
             if (isDev) {
                 console.log(`Replace host to local in ${filepath}`)
                 newContent = content.split(prodHost).join(localhost)
+                newContent = content.split(localhostClient).join(localhost)
             } else {
                 console.log(`Replace host to production in ${filepath}`)
                 newContent = content.split(localhost).join(prodHost)
+                newContent = content.split(localhostClient).join(prodHost)
             }
             
             fs.writeFile(filepath, newContent, (err) => {
